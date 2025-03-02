a885bee3c9ddd547f08ee2ed2381a9c63ad4c7ab
Darshan Magare <darshan.magare@fortytwo42.in>
2025-03-02 13:31:58 +0530

    added new bollywood api

--- Cinemax/Cinemax/HttpUtilities/MoviesService/MoviesRouter.swift
+++ Cinemax/Cinemax/HttpUtilities/MoviesService/MoviesRouter.swift
@@ -10,22 +10,24 @@ import Alamofire
 
 enum MoviesRouter: HttpRouterProtocol {
     
-    case moviesUpcoming(page:Int)
-    case moviesNowPlaying(page:Int)
-    case moviesTopRated(page:Int)
-    case moviesPopular(page:Int)
-    case movieDetail(movieId:Int)
-    case movieSimilar(similarId:Int,page:Int)
-    case movieSearch(searchText: String,page:Int)
-    case movieVideos(movieId:Int)
-    case moviesByGenres(genreId:Int,page:Int)
-    case tvShows(page:Int)
-    case tvShowDetails(showId:Int)
-    case tvShowsCast(showId:Int)
-    case tvShowsVideos(showId:Int)
-    case tvShowSimilar(similarId:Int,page:Int)
-    case tvShowSearch(searchText: String,page:Int)
-    case tvShowByGenres(genreId:Int,page:Int)
+    case moviesUpcoming(page: Int)
+    case moviesNowPlaying(page: Int)
+    case moviesTopRated(page: Int)
+    case moviesPopular(page: Int)
+    case movieDetail(movieId: Int)
+    case movieSimilar(similarId: Int, page: Int)
+    case movieSearch(searchText: String, page: Int)
+    case movieVideos(movieId: Int)
+    case moviesByGenres(genreId: Int, page: Int)
+    case tvShows(page: Int)
+    case tvShowDetails(showId: Int)
+    case tvShowsCast(showId: Int)
+    case tvShowsVideos(showId: Int)
+    case tvShowSimilar(similarId: Int, page: Int)
+    case tvShowSearch(searchText: String, page: Int)
+    case tvShowByGenres(genreId: Int, page: Int)
+    
+    static let API_KEY = "38a73d59546aa378980a88b645f487fc"
     
     var baseUrlString: String {
         return "https://api.themoviedb.org/3"
@@ -34,39 +36,55 @@ enum MoviesRouter: HttpRouterProtocol {
     var path: String {
         switch self {
         case .moviesUpcoming(let page):
-            return "/movie/upcoming?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
+            return "/discover/movie?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=release_date.desc&page=\(page)"
+            
         case .moviesNowPlaying(let page):
-            return "/movie/now_playing?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
+            return "/discover/movie?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=now_playing&page=\(page)"
+            
         case .moviesTopRated(let page):
-            return "/movie/top_rated?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
+            return "/discover/movie?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=vote_average.desc&vote_count.gte=100&page=\(page)"
+            
         case .moviesPopular(let page):
-            return "/movie/popular?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
+            return "/discover/movie?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=popularity.desc&page=\(page)"
+            
         case .movieDetail(let movieId):
-            return "/movie/\(movieId)?api_key=38a73d59546aa378980a88b645f487fc&language=en-US"
-        case .movieSimilar(let similarId , let page):
-            return "/movie/\(similarId)/similar?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
-        case .movieSearch(let searchText , let page):
-            return "/search/movie?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)&query=\(searchText)"
+            return "/movie/\(movieId)?api_key=\(MoviesRouter.API_KEY)&language=hi-IN"
+            
+        case .movieSimilar(let similarId, let page):
+            return "/movie/\(similarId)/similar?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&page=\(page)"
+            
+        case .movieSearch(let searchText, let page):
+            return "/search/movie?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&page=\(page)&query=\(searchText)&with_original_language=hi"
+            
         case .movieVideos(let movieId):
-            return "/movie/\(movieId)/videos?api_key=38a73d59546aa378980a88b645f487fc&language=en-US"
+            return "/movie/\(movieId)/videos?api_key=\(MoviesRouter.API_KEY)&language=hi-IN"
+            
+        case .moviesByGenres(let genreId, let page):
+            return "/discover/movie?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=popularity.desc&page=\(page)&with_genres=\(genreId)"
+            
         case .tvShows(let page):
-            return "/tv/on_the_air?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
+            return "/discover/tv?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=popularity.desc&page=\(page)"
+            
         case .tvShowDetails(let showId):
-            return "/tv/\(showId)?api_key=38a73d59546aa378980a88b645f487fc&language=en-US"
+            return "/tv/\(showId)?api_key=\(MoviesRouter.API_KEY)&language=hi-IN"
+            
         case .tvShowsCast(let showId):
-            return "/tv/\(showId)/credits?api_key=38a73d59546aa378980a88b645f487fc"
+            return "/tv/\(showId)/credits?api_key=\(MoviesRouter.API_KEY)"
+            
         case .tvShowsVideos(let showId):
-            return "/tv/\(showId)/videos?api_key=38a73d59546aa378980a88b645f487fc&language=en-US"
-        case .tvShowSimilar(let similarId,let page):
-            return "/tv/\(similarId)/similar?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)"
-        case .tvShowSearch(let searchText,let page):
-            return "/search/tv?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&page=\(page)&query=\(searchText)"
-        case .moviesByGenres(let genreId,let page):
-            return "/discover/movie?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=\(page)&with_genres=\(genreId)"
-        case .tvShowByGenres(let genreId,let page):
-            return "/discover/tv?api_key=38a73d59546aa378980a88b645f487fc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=\(page)&with_genres=\(genreId)"
+            return "/tv/\(showId)/videos?api_key=\(MoviesRouter.API_KEY)&language=hi-IN"
+            
+        case .tvShowSimilar(let similarId, let page):
+            return "/tv/\(similarId)/similar?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&page=\(page)"
+            
+        case .tvShowSearch(let searchText, let page):
+            return "/search/tv?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&page=\(page)&query=\(searchText)&with_original_language=hi"
+            
+        case .tvShowByGenres(let genreId, let page):
+            return "/discover/tv?api_key=\(MoviesRouter.API_KEY)&language=hi-IN&region=IN&with_original_language=hi&with_origin_country=IN&sort_by=popularity.desc&page=\(page)&with_genres=\(genreId)"
         }
     }
+    
     
     var method: HTTPMethod {
         return .get
@@ -83,8 +101,8 @@ enum MoviesRouter: HttpRouterProtocol {
     func body() throws -> Data? {
         return nil
     }
-    
 }
+
 
 // "https://api.themoviedb.org/3/movie/1011985/videos?api_key=38a73d59546aa378980a88b645f487fc&language=en-US")!
 //        let trailerURL = URL(string: "https://www.youtube.com/watch?v=69yHznzqCEI")!
